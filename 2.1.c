4ac6b9170d106c0bb2e2df76dc0a796b2ea37ba5
Clindo Devassy K <clinddo@gmail.com>
2015-11-07 23:30:03 +0100

    updated as per lab requirement
    compile it with gcc pthread command.
    
    Eg: gcc -pthread 2.1.c

--- 2.1.c
+++ 2.1.c
@@ -1,26 +1,43 @@
-/*#include <stdio.h>
-#include<pthread.h>
+#include <stdio.h>
+#include<pthread.h>  //header file for thread functions
 
-//#include "types.h"
-
-#define MAXLINE 50
 
 void* my_thread_function(void *arg);
+
 int main(void)
 {
-pthread_t my_thread_id;
-int err,err1;
-err = pthread_create(&my_thread_id, NULL, my_thread_function, NULL);
+	pthread_t my_thread_id;
+	fprintf(stderr,"Inside Main Thread\n");
+	int err,err1;
+	err = pthread_create(&my_thread_id, NULL, my_thread_function, NULL);  //create a new thread
+	
+	if(err==0) //Thread creation is successfull
+	{
+		err1 = pthread_join(my_thread_id, NULL);  //main thread waits till new thread is executed.
+
+		if(err1==0)  // Thread joining successfull
+		{
+			fprintf(stderr,"Thread joined successfully");
+		}
+		else
+		{	
+			fprintf(stderr,"Thread join failed");
+		}
+	}
+	else
+	{
+		fprintf(stderr,"Thread creation failed");
+	}
 
 
-err1 = pthread_join(my_thread, NULL);
-return 0;
+	return 0;
 }
 
-void* my_thread_function(void *arg) {
+void* my_thread_function(void *arg) 
 
-fprintf(stderr,"inside Thread");
-pthread_exit(0);
+{
+
+	fprintf(stderr,"Inside Thread \n");
+	pthread_exit(0);
 
 }
-*/
