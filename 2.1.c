66c27d76aa7ff235b50a65a31f7477fa5ad60984
Clindo Devassy K <clinddo@gmail.com>
2015-11-08 21:01:58 +0100

    customized as per question
    please run it with gcc -pthread 2.1.c
    
    Not updated in make file

--- 2.1.c
+++ 2.1.c
@@ -3,21 +3,28 @@
 
 
 void* my_thread_function(void *arg);
-
+int my_value=42;
 int main(void)
 {
 	pthread_t my_thread_id;
 	fprintf(stderr,"Inside Main Thread\n");
 	int err,err1;
+	fprintf(stderr,"My Value inside main thread before join is :%d \n",my_value);
+	pthread_t id = pthread_self(); // to get thread id of main thread
+
+	fprintf(stderr,"Main thread id %lu\n", id);
+
 	err = pthread_create(&my_thread_id, NULL, my_thread_function, NULL);  //create a new thread
+	usleep(150);
 	
 	if(err==0) //Thread creation is successfull
-	{
+	{	fprintf(stderr,"New Thread's Id is %lu\n", (unsigned long) my_thread_id);  
 		err1 = pthread_join(my_thread_id, NULL);  //main thread waits till new thread is executed.
-
+		
 		if(err1==0)  // Thread joining successfull
 		{
-			fprintf(stderr,"Thread joined successfully");
+			fprintf(stderr,"Thread joined successfully \n");
+			fprintf(stderr,"My Value inside main thread after join :%d \n",my_value);
 		}
 		else
 		{	
@@ -36,8 +43,10 @@ int main(void)
 void* my_thread_function(void *arg) 
 
 {
-
-	fprintf(stderr,"Inside Thread \n");
+	my_value = 18951;
+	fprintf(stderr,"Inside New Thread \n");
+	fprintf(stderr,"My Value inside new thread is : %d \n",my_value);
+	usleep(500);
 	pthread_exit(0);
 
 }
