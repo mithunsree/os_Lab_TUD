f2602ca6e5fba240bce323306792cf80c19526f6
sachin <sachin.manawadi@intimetec.com>
2015-11-10 04:28:37 +0530

    Modified 5.1.c

--- 5.1.c
+++ 5.1.c
@@ -11,77 +11,81 @@
 #include <sys/wait.h>
 
 
-#define MAXLINE 50
+#define MAXLENGTH 100
 #define MYSHMNAME "/DEEDS_lab1_shm"
 
-int glob = 6; /* external variable in initialized data */
+int my_value = 42; /* external variable in initialized data */
 int main(void)
 {
-	//pid_t pid;
-	int n;
-	int pid;
-	char line[MAXLINE];
-	int msgq_id;
-	int status = 0;
-	int r = 0;
-	char name1[MAXLINE];
-	char name2[MAXLINE];
-	const size_t region_size = sysconf(_SC_PAGE_SIZE);
+        //int n;
+        int pid;
+//      char line[MAXLINE];
+        //int msgq_id;
+        int status = 0;
+        int r = 0;
+//      char name1[MAXLINE];
+//      char name2[MAXLINE];
+        const size_t region_size = sysconf(_SC_PAGE_SIZE);
 
-	int fd = shm_open(MYSHMNAME, O_CREAT | O_TRUNC | O_RDWR, 0666);
-  	if (fd == -1)
-    	{
-		fprintf(stderr,"shm_open failed\n");
-	}
+        int fd = shm_open(MYSHMNAME, O_CREAT | O_TRUNC | O_RDWR, 0666);
+        if (fd == -1)
+        {
+	fprintf(stderr,"shm_open failed\n");
+        }
 
-  	r = ftruncate(fd, region_size);
-  	if (r != 0)
-    	{
-		fprintf(stderr,"ftruncate failed\n");
-	}
+        r = ftruncate(fd, region_size);
+        if (r != 0)
+        {
+                fprintf(stderr,"ftruncate failed\n");
+        }
 
-  	void *ptr = mmap(0, region_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
- 	if (ptr == MAP_FAILED)
-    	{
-		fprintf(stderr,"mmap");
-	}
-  	close(fd);
+        void *ptr = mmap(0, region_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+        if (ptr == MAP_FAILED)
+        {
+                fprintf(stderr,"mmap");
+        }
+        close(fd);
 
-	if ((pid = fork()) < 0) 
+        if ((pid = fork()) < 0)
+        {
+                fprintf(stderr,"fork error\n");
+        }
+        usleep(150000);
+
+	if(pid > 0)
 	{
-		fprintf(stderr,"fork error\n");
-	}
+                char *message1 = NULL;
+                message1 = (char *) ptr;
+                sprintf(message1,"Hi, I am your parent. My PID=%d and my_value=%d",getpid(),my_value);
+        }
 	
-	if (pid == 0) {
-		sleep(3);
-    		//u_long *d = (u_long *) ptr;
-    		//*d = 0xdbeebee;
-    		//exit(0);
-		//fprintf(stderr,"Parent wrote:%s\n",*(char *)ptr);
-		fprintf(stderr,"Parent wrote %s\n",(char *) ptr);
-  	}
-	else
+	if( pid == 0 )
 	{
-		int status;
-//    		waitpid(pid, &status, 0);
-    		//printf("child wrote %#lx\n", *(u_long *) ptr);
-		char *name;
-		name = (char *)malloc(sizeof(7));
-//		sprintf(name,"sachin");
-		name = (char *) ptr;
-		sprintf(name,"sachin");
-                //*d = 0xdbeebee;
-		waitpid(pid,&status,0);
-	}
-	
-	 r = munmap(ptr, region_size);
-  	if (r != 0)
-	{
-    		fprintf(stderr,"munmap failed");
+		char *message2 = NULL;
+                usleep(500000);
+                my_value = 18951;
+                fprintf(stderr,"I am the child.Parent wrote:\n %s\n",(char *) ptr);
+                message2 = (char *)ptr;
+                sprintf(message2,"Hi, I am your child. My PID=%d and my_value=%d",getpid(),my_value);
+		exit(1);
 	}
 
-  	r = shm_unlink(MYSHMNAME);
- 	if (r != 0)
-    	fprintf(stderr,"shm_unlink failed");
-  	return 0;	
+	if(pid > 0)
+	{
+		//fprintf(stderr,"I am the Parent.Child wrote:\n %s\n",(char *) ptr);
+        	wait(&status);
+//		if(WIFSTOPPED(status))
+//		{
+			fprintf(stderr,"I am the Parent.Child wrote:\n %s\n",(char *) ptr);	
+//		}
+        	r = munmap(ptr, region_size);
+        	if (r != 0)
+        	{
+                	fprintf(stderr,"munmap failed");
+        	}
+        	r = shm_unlink(MYSHMNAME);
+        	if (r != 0)
+        	fprintf(stderr,"shm_unlink failed");
+	}
+	return 0;
 }
